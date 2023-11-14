656b05f3d2c81427afc2590814cee05e9c09b274
Mark Brewer <markbrewer369@gmail.com>
2023-11-13 21:06:44 -0600

    Models Created

---
+++ Models/Journal.cs
@@ -0,0 +1,17 @@
+﻿namespace LoveLink.Models
+{
+    public class Journal
+    {
+        public int? Id { get; set; }
+        required
+        public int? UserId { get; set; }
+        public int? PartnerId { get; set; }
+        public int? PartnerUid { get; set; }
+        public string? Name { get; set; }
+        public string? Entry { get; set; }
+        public DateTime? DateEntered { get; set; }
+        public string? Visibility { get; set; }
+        public ICollection<MoodTag>? MoodTags { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace LoveLink.Models
+{
+    public class User
+    {
+        public int? Id { get; set; }
+        required
+        public int? UID { get; set; }
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public string? Bio { get; set; }
+        public string? Gender { get; set; }
+        public string? ProfilePhoto { get; set; }
+        public int? PartnerId { get; set; }
+        public int? PartnerUid { get; set; }
+        public DateTime? AnniversaryDate { get; set; }
+        public int? PartnerCode { get; set; }
+        public MyMood? MyMood { get; set; }
+        public ICollection<Journal>? Journals { get; set; }
+        public ICollection<Notification>? Notifications { get; set; }
+        public User? PartnerUser { get; set; }
+    }
+}
