00fd302e846c1ca765b8c1574dcf09a1a3116918
Mark Brewer <markbrewer369@gmail.com>
2024-01-23 20:31:48 -0600

    !

--- Program.cs
+++ Program.cs
@@ -594,7 +594,7 @@ app.MapGet("/userNotifs/{id}", async (LoveLinkDbContext db, int id) =>
 
     return notifications;
 });
-
+//DELETE Notifications
 app.MapDelete("/deleteNotif/{notificationId}", async (LoveLinkDbContext db, int notificationId) =>
 {
     var notification = await db.Notifications
@@ -613,7 +613,7 @@ app.MapDelete("/deleteNotif/{notificationId}", async (LoveLinkDbContext db, int 
         return Results.NotFound();
     }
 });
-
+//Set Unviewed Notifications to Viewed
 app.MapPut("/setNotifsViewed", async (LoveLinkDbContext db, HttpRequest request) =>
 {
     try
@@ -664,7 +664,7 @@ app.MapPut("/setNotifsViewed", async (LoveLinkDbContext db, HttpRequest request)
         return Results.BadRequest($"Error updating notifications: {ex.Message}");
     }
 });
-
+//GET unviewed notifications
 app.MapGet("/unviewedNotifs/{id}", (LoveLinkDbContext db, int id) =>
 {
     try
