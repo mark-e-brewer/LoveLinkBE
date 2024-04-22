aae5c08acd23845a4f46a138729b26ea9429905a
Mark Brewer <markbrewer369@gmail.com>
2024-04-22 01:54:42 -0500

    new

--- Program.cs
+++ Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddCors(options =>
 });
 
 // Add services to the container.
+//New
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
